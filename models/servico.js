5f914c2c12fc36d4cb611ae38aefc91b74bfc655
hayeknizam <vinicius.andrei@live.com>
2021-10-13 17:30:23 -0300

    gerando migrações

--- models/servico.js
+++ models/servico.js
@@ -9,6 +9,15 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Servico.belongsToMany(models.Pedido, {
+        foreignKey: 'PedidoId',
+        through: 'ItemPedido',
+        as: 'pedidos_serv',
+      });
+      Servico.hasMany(models.ItemPedido, {
+        foreignKey: 'ServicoId',
+        as: 'item_servicos',
+      });
     }
   }
   Servico.init(
